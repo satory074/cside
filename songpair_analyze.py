f0232537a82050848c396e0b7372930f9fdd2da4
satory43 <strt43.0316@gmail.com>
2018-07-25 00:02:15 +0900

    [del]def find segstars

--- songpair_analyze.py
+++ songpair_analyze.py
@@ -16,15 +16,12 @@ class SongPair:
         print(self.oti)
 
         self.song1.htr = np.roll(self.song1.h, self.oti, axis=0)
-        print
-
         print ("[{}]-[{}]".format(self.song1.filename, self.song2.filename))
 
         self.crp_R = self._calc_R(self.song1.htr, self.song2.h, self.CPR)
-
-        #self.crp_L, self.Lmax, self.segstart_L, self.segends_L = self._calc_L()
-        #self.crp_S, self.Smax, self.segstarts_S, self.segends_S = self._calc_SQ()
-        self.crp_Q, self.Qmax, self.segstarts_Q, self.segends_Q = self._calc_SQ(gamma_o=5.0, gamma_e=0.5)
+        #self.crp_L, self.Lmax, self.segends_L = self._calc_L()
+        #self.crp_S, self.Smax, self.segends_S = self._calc_SQ()
+        self.crp_Q, self.Qmax, self.segends_Q = self._calc_SQ(gamma_o=5.0, gamma_e=0.5)
 
     def _calcOTI(self, ga, gb):
         oti = 0
@@ -93,82 +90,6 @@ class SongPair:
         print
         return crp_L, Lmax
 
-
-    "legacy find start point of segment algorythm"
-    def findsegstartSQ(self, crp_R, crp_SQ, SQmax, segends, gamma_o, gamma_e):
-        print ("######find SQstart...")
-        segstarts = [] # segment start points
-        junction = []
-        history = [] # already searched points
-        for segend in segends:
-            pivot_x, pivot_y = segend
-            pivotSQmax = SQmax
-
-            is_search = True
-            ntry = 0
-            while is_search == True:
-                if (pivot_x, pivot_y, pivotSQmax) in history:
-                    if len(junction) == 0:
-                        is_search = False
-                        break
-                    else:
-                        pivot_x, pivot_y, pivotSQmax = junction.pop(0)
-
-                if pivotSQmax == 0:
-                    if (pivot_x, pivot_y) not in segstarts:
-                        segstarts.append((pivot_x, pivot_y))
-
-                    if len(junction) == 0:
-                        is_search = False
-                        break
-                    else:
-                        pivot_x, pivot_y, pivotSQmax = junction.pop(0)
-                        continue
-
-                elif pivotSQmax < 0:
-                    print (pivotSQmax)
-                    print ("pivotSQmax is zero!!")
-                    exit()
-
-                history.append((pivot_x, pivot_y, pivotSQmax))
-                look = [(pivot_x - 1, pivot_y - 1),
-                    (pivot_x - 2, pivot_y - 1),
-                    (pivot_x - 1, pivot_y - 2)]
-
-                candidate = []
-                for (i, j) in look:
-                    if crp_R[pivot_x][pivot_y] == 1:
-                        calcedSQmax = pivotSQmax - 1
-                    else:
-                        calcedSQmax = pivotSQmax + (gamma_o if crp_R[i][j] == 1 else gamma_e)
-
-                    if calcedSQmax == crp_SQ[i][j]:
-                        candidate.append((i, j, calcedSQmax))
-
-                if len(candidate) == 0:
-                    print (ntry)
-                    print ("candidate list is enpty! Nandeya!!")
-                    exit()
-
-                pivot_x, pivot_y, pivotSQmax = candidate.pop(0)
-
-                if len(candidate) != 0:
-                    junction += candidate
-
-                ntry += 1
-
-
-                if ntry % 5000 == 0:
-                    print ("    " + str(ntry) + " times: " + str(len(junction)) + " junctions.")
-                # end condition
-                if ntry == 100000000:
-                    print ("Run Time Error.")
-                    print (len(junction))
-                    exit()
-
-        return segstarts
-
-
     def _calc_SQ(self, gamma_o=float("inf"), gamma_e=float("inf")):
         matlabel = 'S' if gamma_o == float("inf") else 'Q'
         print ("### matrix " + matlabel + " calculate...")
@@ -235,11 +156,7 @@ class SongPair:
                     segstarts.append(start_)
             segends.append((x, y))
 
-        #segstarts = self.findsegstartSQ(self.crp_R, crp_SQ, SQmax, segends, gamma_o, gamma_e)
-
-
         print matlabel + "max: " + str(SQmax)
-        #print matlabel + "start: " + str(segstarts)
         print matlabel + "end: " + str(segends)
         print
         return crp_SQ, SQmax, segstarts, segends
